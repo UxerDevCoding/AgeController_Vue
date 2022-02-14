d856f3964dcdc79de10269434d5bb84cde1b1f8f
UxerDevCoding <omarheviaarbana@gmail.com>
2022-02-14 20:58:21 +0100

    vue age controller styled

---
+++ src/components/AgeController.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h2>Age Controller</h2>
+    <input type="text" placeholder="Escribe tu edad" id="ageInput" required />
+    <button v-on:click="userAge()">Comprobar edad</button>
+    <div id="ageController"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AgeController",
+  data: () => ({}),
+
+  methods: {
+    userAge() {
+      let age = parseInt(document.getElementById("ageInput").value);
+      let printResult = document.getElementById("ageController");
+      printResult.innerHTML = "<p>";
+      if (age) {
+        if (age >= 18) {
+          printResult.innerHTML += "Tienes " + age + " años y puedes conducir";
+          return;
+        }
+
+        printResult.innerHTML +=
+          "Tienes " + age + " años y no tienes permitido conducir";
+        return;
+      }
+
+      printResult.innerHTML += "Debes introducir tu edad";
+    },
+  },
+};
+</script>
+
+<style>
+.v-main__wrap {
+  display: flex;
+  max-width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.v-main {
+  background-color: #e3e6f1;
+}
+
+#ageInput {
+  display: inline-flex;
+  background-color: #ffffff;
+  align-items: center;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  font-size: 1rem;
+  font-family: "Lucida Sans Regular";
+  box-shadow: 0px 2px 4px 1px rgba(141, 141, 141, 0.5);
+  padding: 10px;
+}
+
+button {
+  padding: 10px;
+  background-color: rgb(59, 212, 212);
+  margin-left: 10px;
+  font-size: 1rem;
+  border-radius: 0.5rem;
+  font-family: "Lucida Sans";
+  font-weight: bold;
+  box-shadow: 0px 2px 4px 1px rgba(0, 0, 0, 0.25);
+}
+
+h2 {
+  margin-bottom: 60px;
+  text-align: center;
+  font-family: "Lucida Sans Regular";
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+}
+
+#ageController {
+  margin-top: 30px;
+  font-family: "Lucida Sans Regular";
+}
+</style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>Omar Hevia</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FooterBar",
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.theme--light.v-sheet {
+  background-color: rgba(59, 212, 212);
+  border-color: #4caac7;
+  color: #ffffffec;
+}
+</style>
